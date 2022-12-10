ab26a3ca74e963d3c886ef35e08700d06896dbf8
MZ1840 <MZ1840>
2022-12-10 02:51:20 -0800

    low

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
- * main- Evaluate if a number is +ve or -ve
+/**
+ * main - positive, negative or zero
  *
- *Return 0 on success otherwise 1
+ * Return: 0
  *
  */
-int main (void)
+int main(void)
 {
 	int n;
+
 	srand(time(0));
-	n=rand() - RAND_MAX / 2;
-	if(n > 0)
-	
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
 		printf("%d is positive\n", n);
-	
-	else if(n == 0)
-	
+
+	else if (n == 0)
 		printf("%d is zero\n", n);
-		
 	else
-	
 		printf("%d is negative\n", n);
-	
+
 	return (0);
 }
