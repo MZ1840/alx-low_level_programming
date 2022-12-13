0c1c180801eefdce88ddc7a05c2f580303a05f56
MZ1840 <MZ1840>
2022-12-13 02:27:03 -0800

    loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: 0 
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 97; i <= 122; i++)
+
+	{
+
+		_putchar(i);
+
+	}
+
+	_putchar('\n');
+	return (0);
+}
